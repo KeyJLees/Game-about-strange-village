f51d4070991ebbedbd2375f3c3335decb3e762b1
unknown <glebsolomin@mail.ru>
2020-06-09 09:39:04 +0700

    'map'

---
+++ MyControlWork/Assets/myscriptsforgame/backtomenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class backtomenu : MonoBehaviour
+{
+    public Button Back;
+    void Start()
+    {
+        Back.onClick.AddListener(toMenu);
+    }
+
+    void toMenu()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+
+}
---
+++ MyControlWork/Assets/myscriptsforgame/mainmenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class mainmenu : MonoBehaviour
+{
+    public Button Play;
+    public Button Settings;
+    public Button Exit;
+    
+    void Start()
+    {
+        Play.onClick.AddListener(toGame);
+        Settings.onClick.AddListener(toSettings);
+        Exit.onClick.AddListener(toExit);
+    }
+
+    void toExit()
+    {
+        Application.Quit();
+    }
+
+    void toGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void toSettings()
+    {
+        SceneManager.LoadScene("settings");
+    }
+}
